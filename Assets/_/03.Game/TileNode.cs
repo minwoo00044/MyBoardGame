d8254895ae01b9b68da0234bbf8022d0f4fca391
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2024-06-17 17:20:58 +0900

    면접 연락으로 잠시 중단

--- Assets/_/03.Game/TileNode.cs
+++ Assets/_/03.Game/TileNode.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class TileNode : MonoBehaviour
 {
     public bool isTouchble = false;
     Transform player;
     Player playerMovement;
+    PhotonView view;
 
     private void Start()
     {
+        foreach(var item in FindObjectsByType<PhotonView>(sortMode: FindObjectsSortMode.None))
+        {
+            if(item.IsMine)
+                view = item;
+        }
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerMovement = player.GetComponent<Player>();
     }
